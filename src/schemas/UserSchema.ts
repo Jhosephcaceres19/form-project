0f744e71146f9056b536603992835f9b26b3a499
Jhoseph <jhoseph@Jhoseph.Jhoseph>
2025-08-13 12:21:37 -0400

    add first commit

---
+++ src/schemas/UserSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const UserSchema = z
+  .object({
+    name: z.string().min(1, "El nombre es requerido"),
+    email: z.email("Email no es valido").min(1, "Email es requerido"),
+    password: z.string().min(6, "Debe tener almenos 6 caracteres"),
+    confimrPasswornd: z.string().min(6, "Debe tener almenos 6 caracteres"),
+  })
+  .refine((data) => data.password === data.confimrPasswornd, {
+    path: ["Confirmado password"],
+    message: "Las contrasenas son correctas",
+  });
+
+export type UserSchemaType = z.infer<typeof UserSchema>;
+
+export const dafaultValues: UserSchemaType = {
+  name: " ",
+  email: " ",
+  password: " ",
+  confimrPasswornd: " ",
+};
